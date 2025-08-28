45141ae61524b6cfde07122e2cf27b566b3c4cae
stonezdj <stonezdj@gmail.com>
2025-08-28 16:39:42 +0800

    fix issue
    Signed-off-by: stonezdj <stonezdj@gmail.com>

--- magefile.go
+++ magefile.go
@@ -348,7 +348,7 @@ func addVusAndIterationsArgs(args []string) []string {
 		args = append(args, "--iterations", strconv.FormatInt(iterations, 10))
 	}
 
-	// add duration to avoid k6 error if both vus and iterations are not set
+	// add duration to avoid k6 error
 	args = append(args, "-d", "60m")
 
 	return args
